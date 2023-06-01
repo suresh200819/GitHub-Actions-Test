9e868501896c697109c63a23ae6d28cc08cc62a8
suresh200819 <79845886+suresh200819@users.noreply.github.com>
2023-06-01 16:25:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ resource "google_compute_address" "public_ip" {
 }
 
 resource "google_compute_instance" "gce_instance" {
-  project      = "nifty-quanta-387606"
   name         = var.instance_name
   machine_type = var.machine_type
   zone         = var.zone
